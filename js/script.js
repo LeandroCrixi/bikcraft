f8bfc4521b0b624acb8a3ed30b230c927925e388
Leandro Crixi <leandro.crixi@hotmail.com>
2023-05-22 22:11:28 +0100

    20230522

---
+++ js/script.js
@@ -0,0 +1,40 @@
+// Ativar links do menu
+const links = document.querySelectorAll('.header-menu a')
+
+function ativarLink(link){
+    const url = window.location.href;
+    const href = link.href;
+
+    if(url.includes(href)){
+        link.classList.add('ativo')
+    }
+}
+
+links.forEach(ativarLink)
+
+// Ativar items do orcamento
+const parametros = new URLSearchParams(location.search);
+function ativarProduto(parametro){
+    const elemento = document.getElementById(parametro)
+    if(elemento){
+        elemento.checked = true;
+        console.log(elemento)
+    }
+}
+parametros.forEach(ativarProduto);
+
+// Perguntas Frequentes
+const perguntas = document.querySelectorAll('.perguntas button')
+function ativarPergunta(event){
+    const pergunta = event.currentTarget;
+    const controls = pergunta.getAttribute('aria-controls')
+    const resposta = document.getElementById(controls)
+    resposta.classList.toggle('ativa')
+    const ativa = resposta.classList.contains('ativa')
+    pergunta.setAttribute('aria-expanded', ativa)
+    console.log(ativa)
+}
+function eventosPerguntas(pergunta){
+    pergunta.addEventListener('click', ativarPergunta);
+}
+perguntas.forEach(eventosPerguntas)
